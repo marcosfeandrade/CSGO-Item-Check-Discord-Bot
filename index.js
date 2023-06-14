9e1dd570cbde5609a0fae44bdcf3fff081f7cfb7
Marcos Fellipe Andrade da Silva <marcos.silva@raidhut.com.br>
2023-06-14 18:15:45 -0300

    Fix embed message and add item color

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const request = require("./request.js");
-const { Client } = require("discord.js");
+const { Client, MessageEmbed } = require("discord.js");
 const client = new Client({
   intents: 3276799,
 });
 const config = require("./config.json");
+
 client.on("ready", () => {
   console.log("Online");
 });
@@ -16,16 +17,29 @@ client.on("messageCreate", async (message) => {
   const [name, time, currency] = splitarg;
 
   request
-    .getData(name, time, currency)
+    .getItemData(name, time, currency)
     .then((resp) => {
-      if (resp.data.success == "false" || !name || !time || !currency)
-        return message.reply(
-          "Error. The correct structure: $info item(english) // time(days) // currency \nE.g.\nInput: $info AK-47 | Aquamarine Revenge (Battle-Scarred) // 7 // USD\nOutput: \nAK-47 | Aquamarine Revenge (Battle-Scarred)\nInformation at last 7 day(s)\nAverage price = USD 15.58;\nAmount sold = 373;\nLowest price = USD 13.99;\nHighest price= USD 17.82;"
-        );
+      if (resp.data.success == "false" || !name || !time || !currency) {
+        const errorMessage = new MessageEmbed()
+          .setColor("#ff0000")
+          .setTitle("Erro")
+          .setDescription(
+            "Estrutura correta: $info item(english) // time(days) // currency\n\nExemplo:\nInput: $info AK-47 | Aquamarine Revenge (Battle-Scarred) // 7 // USD\n\nSaída:\nAK-47 | Aquamarine Revenge (Battle-Scarred)\nInformações dos últimos 7 dias\nPreço médio = USD 15.58;\nQuantidade vendida = 373;\nMenor preço = USD 13.99;\nMaior preço = USD 17.82;"
+          );
 
-      message.reply(
-        `${name}\nInformations of Steam at last ${time} day(s)\nAverage price = ${currency} ${resp.data.average_price};\nAmount sold = ${resp.data.amount_sold};\nLowest price = ${currency} ${resp.data.lowest_price};\nHighest price= ${currency} ${resp.data.highest_price}.`
-      );
+        return message.reply({ embeds: [errorMessage] });
+      }
+
+      const embedMessage = new MessageEmbed()
+        .setColor("#" + request.getItemColor(name))
+        .setTitle(name)
+        .setDescription(`Informações do Steam nos últimos ${time} dia(s)`)
+        .addFields("Preço médio", `${currency} ${resp.data.average_price}`,
+        "Quantidade vendida", resp.data.amount_sold,
+        "Menor preço", `${currency} ${resp.data.lowest_price}`,
+        "Maior preço", `${currency} ${resp.data.highest_price}`);
+
+      message.reply({ embeds: [embedMessage] });
     })
     .catch((err) => {
       console.error(err);
--- request.js
+++ request.js
@@ -1,9 +1,8 @@
 var axios = require("axios");
 //http://csgobackpack.net/api/GetItemsList/v2/
 //https://csgobackpack.net/api/GetItemPrice/?currency=USD&id=${name}&time=${time}
-var response;
 
-async function getData(name, time, currency) {
+async function getItemData(name, time, currency) {
   try {
     return await axios.get(
       `https://csgobackpack.net/api/GetItemPrice/?currency=${currency}&id=${name}&time=${time}`
@@ -13,6 +12,19 @@ async function getData(name, time, currency) {
   }
 }
 
+async function getItemColor(name) {
+  try {
+    const response = await axios.get(
+      `https://csgobackpack.net/api/GetItemsList/v2/`
+    );
+    const itemName = response
+    return itemName.data.items_list["AK-47 | Aquamarine Revenge (Battle-Scarred)"].rarity_color;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 module.exports = {
-  getData,
+  getItemData,
+  getItemColor
 };
